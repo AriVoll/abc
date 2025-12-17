64f4dcb86e775bc32bf0a88ec432b7da24a1301e
AriVoll <volosh_2021@mail.ru>
2025-12-17 13:48:30 +0000

    Add files via upload

---
+++ shell.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <string>
+#include<fstream>
+
+int main() {
+    // Flush after every std::cout / std:cerr
+    std::cout << std::unitbuf;
+    std::cerr << std::unitbuf;
+
+
+    std::cout << "$ ";
+
+    const char* home=std::getenv("HOME");
+    std::string historyPath=std::string(home)+"/.kubsh_history";
+
+    std::string input;
+    //std::getline(std::cin, input);
+    while (std::getline(std::cin, input))
+    {
+        std::ofstream history(historyPath,std::ios::app);
+        history << input<<"\n";
+        std::cout << "$ ";
+        if (input == "\\q")
+            break;
+        if (input.substr(0, 5) == "echo ")
+        {
+            std::cout << input.substr(5);
+        }
+        else
+        {
+            std::cout << input << ": command not found\n";
+        }
+    }
+}
